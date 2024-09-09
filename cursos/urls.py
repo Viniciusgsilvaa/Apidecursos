139c01544310495a0b8b78e7e6026eb5eda7a57a
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-09-09 18:16:02 +0100

    Criando views para o api

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import AvaliacoesApiView, CursosApiView, ProfessorApiView
 
 urlpatterns = [
-
+    path('avaliacoes/', AvaliacoesApiView.as_view(), name='avaliacoes'),
+    path('cursos/', CursosApiView.as_view(), name='cursos'),
+    path('professor', ProfessorApiView.as_view(), name='professor')
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
 
-# Create your views here.
+from .models import Avaliacao, Curso, Professor
+from.serializers import AvaliacaoSerializer, CursoSerializer, ProfessorSerializer
+
+
+class AvaliacoesApiView(APIView):
+    """
+    API Avaliacoes
+    """
+    def get(self, request):
+        avaliacao = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacao, many=True)
+        return Response(serializer.data)
+
+
+class CursosApiView(APIView):
+    
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+
+class ProfessorApiView(APIView):
+
+    def get(self, request):
+        professor = Professor.objects.all()
+        serializer = ProfessorSerializer(professor, many=True)
+        return Response(serializer.data)
